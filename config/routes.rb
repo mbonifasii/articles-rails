ed3355923065c3385dd9d71e5d34ff42b69d797d
Marcos <marcosbonifasi19@gmail.com>
2021-11-07 23:46:18 -0600

    feat: add actions methods for articles

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,20 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  get "/hello", to: "hello#index"
+  root to: "home#index"
+
+  get "hello", to: "hello#index"
+
+  get "articles", to: "articles#new"
+
+  post "articles", to: "articles#create"
+
+  get "articles/:id", to: "articles#show"
+
+  get "articles/:id/edit", to: "articles#edit"
+
+  patch "articles/:id", to: "articles#update", as: :article
+
+  delete "articles/:id", to: "articles#destroy"
 
 end
